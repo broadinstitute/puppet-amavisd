41936d8d1715908a5902c6b57c53db24a644acbb
Andrew Teixeira <teixeira@broadinstitute.org>
2016-05-12 16:58:03 -0400

    Start making some basic tests work

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,40 @@
 require 'spec_helper'
+
 describe 'amavisd' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('amavisd') }
+  [ 'Debian', 'RedHat' ].each do |osfamily|
+
+    context "on #{osfamily}" do
+
+      if osfamily == 'Debian'
+        let(:facts) { {
+          :osfamily               => 'Debian',
+          :operatingsystem        => 'Debian',
+          :lsbdistid              => 'Debian',
+          :lsbdistcodename        => 'wheezy',
+          :kernelrelease          => '3.2.0-4-amd64',
+          :operatingsystemrelease => '7.3',
+          :operatingsystemmajrelease => '7',
+        } }
+
+        context 'with defaults for all parameters' do
+          it { should contain_class('amavisd') }
+        end
+      end
+
+      if osfamily == 'RedHat'
+        let(:facts) { {
+          :osfamily => osfamily,
+          :operatingsystem => 'RedHat',
+          :operatingsystemrelease => '6.5',
+          :operatingsystemmajrelease => '7',
+        } }
+
+        context 'with defaults for all parameters' do
+          it { should contain_class('amavisd') }
+        end
+      end
+
+    end
   end
 end
